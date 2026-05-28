50443e403d025d57257bc3702949551b9827a917
G00se2 <ebryantbrown03@gmail.com>
2026-05-28 13:51:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-let weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
-
-
 let today = new Date();
 let hrs = today.getHours();
-let min = today.getMinutes();
-let hours = `${hrs}:${min < 10 ? '0' + min : min}`
-let dayOfWeek = weekday[today.getDay()];
-let date = dayOfWeek + " " + today.getDate() + "/" + (today.getMonth() + 1) + '/' + today.getFullYear();
-
 let greet;
 
-if (hrs < 12)
-    greet = 'Good Morning!';
-else if (hrs >= 12 && hrs <= 17)
-    greet = 'Good Afternoon!';
-else if (hrs >= 17 && hrs <= 24)
-    greet = 'Good Evening!';
+if (hrs < 12) {
+    greet = 'Good Morning.';
+} else if (hrs >= 12 && hrs < 17) {
+    greet = 'Good Afternoon.';
+} else {
+    greet = 'Good Evening.';
+}
 
-document.getElementById('widget').innerHTML =
-    greet += `<div id="today">${date} - ${hours}</div>`;
+document.getElementById('widget').innerHTML = greet;
